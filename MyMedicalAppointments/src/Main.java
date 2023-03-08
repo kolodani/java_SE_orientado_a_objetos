934b5523979ed3d7340c654dd473f7198b8a95fd
kolodani <danielvinzia@gmail.com>
2023-03-08 00:49:31 -0300

    java, metodo constructor

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Grover Cleveland");
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
     }
