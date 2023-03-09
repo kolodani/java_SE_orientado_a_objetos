7748ff8319145eb4ce22a51603d1f7822fc44e10
kolodani <danielvinzia@gmail.com>
2023-03-08 23:28:49 -0300

    java, creando elementos estaticos

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,8 +1,18 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Grover Cleveland");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+        // System.out.println(Doctor.id);
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+        // System.out.println(Doctor.id);
+
+        showMenu();
     }
 }
