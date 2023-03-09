e59f6f142956c8084ee904562d3377217198fb6a
kolodani <danielvinzia@gmail.com>
2023-03-09 00:04:24 -0300

    java, primer reto superado

---
+++ MyMedicalAppointments/src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+    public String name;
+    public String email;
+    public String address;
+    public String phoneNumber;
+    public String birthday;
+    public Double weight;
+    public Double height;
+    public String blood;
+
+    public Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
