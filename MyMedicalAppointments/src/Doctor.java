6a73c1aa2699860f90fb39ff901efe0fc6952944
kolodani <danielvinzia@gmail.com>
2023-03-08 23:55:43 -0300

    java, sobrecarga de metodos y constructores

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -6,11 +6,13 @@ public class Doctor {
 
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
-        id++;
     }
 
-    Doctor(String name) {
+    Doctor(String name, String speciality) {
         System.out.println("El nombre del Doctor asignado es: " + name);
+        id++;
+        this.name = name;
+        this.speciality = speciality;
     }
 
     // Comportamientos
