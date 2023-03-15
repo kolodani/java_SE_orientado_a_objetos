2b88891f8d8147da763095ccd4e7c00a4b2f44e1
kolodani <danielvinzia@gmail.com>
2023-03-15 16:18:47 -0300

    java, super y this

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,34 +1,25 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
+public class Doctor extends User {
     // Atributos
-    static int id = 0; // Autoincrement
-    private String name;
-    private String email;
     private String speciality;
 
-    // Avaiable Appointment
-
-    Doctor() {
-        System.out.println("Construyendo el objeto Doctor");
+    Doctor(String name, String email) {
+        super(name, email);
+        System.out.println("El nombre del Doctor asignado es: " + name);
     }
 
-    Doctor(String name, String speciality) {
-        System.out.println("El nombre del Doctor asignado es: " + name);
-        id++;
-        this.name = name;
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
         this.speciality = speciality;
     }
+    // Avaiable Appointment
 
     // Comportamientos
-    public void showName() {
-        System.out.println(name);
-    }
-
-    public void showId() {
-        System.out.println("ID Doctor: " + id);
-    }
 
     ArrayList<AvailableAppointment> AvailableAppointments = new ArrayList<>();
 
--- MyMedicalAppointments/src/Patient.java
+++ MyMedicalAppointments/src/Patient.java
@@ -1,18 +1,12 @@
-public class Patient {
+public class Patient extends User {
     // Atributos
-    int id;
-    private String name;
-    private String email;
-    private String address;
-    private String phoneNumber;
     private String birthday;
     private Double weight;
     private Double height;
     private String blood;
 
     public Patient(String name, String email) {
-        this.name = name;
-        this.email = email;
+        super(name, email);
     }
 
     public void setWeight(double weight) {
@@ -29,24 +23,5 @@ public class Patient {
 
     public String getHeight() {
         return this.height + " Mts.";
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        if (phoneNumber.length() > 8) {
-            System.out.println("El numero telefonico debe ser de 8 digitos maximo");
-        } else if (phoneNumber.length() == 8) {
-            this.phoneNumber = phoneNumber;
-        }
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getName() {
-        return name;
     }
 }
