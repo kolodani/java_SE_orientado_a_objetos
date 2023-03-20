7f3025edfd0d03d8f9e1e835994f241403d20a10
kolodani <danielvinzia@gmail.com>
2023-03-20 11:21:29 -0300

    java, recorriendo estructuras de arbol en java

--- MyMedicalAppointments/src/model/Doctor.java
+++ MyMedicalAppointments/src/model/Doctor.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class Doctor extends User {
     // Atributos
     private String speciality;
+    private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
 
     public Doctor(String name, String email) {
         super(name, email);
@@ -24,7 +25,6 @@ public class Doctor extends User {
 
     // Comportamientos
 
-    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
 
     public void addAvailableAppointment(String date, String time) {
         availableAppointments.add(new Doctor.AvailableAppointment(date, time));
@@ -68,11 +68,11 @@ public class Doctor extends User {
             this.id = id;
         }
 
-        public Date getDate() {
+        public Date getDate(String DATE) {
             return date;
         }
 
-        public String getDate(String DATE) {
+        public String getDate() {
             return format.format(date);
         }
 
